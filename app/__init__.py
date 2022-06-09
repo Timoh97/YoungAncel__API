d76445b06ed92c2bcfd172683f2e70fb230b4df8
Timoh97 <timohmugendi@gmail.com>
2022-06-10 02:11:10 +0300

    link config.py to app via config_options

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import config_options
 
 bootstrap=Bootstrap
 
@@ -8,6 +9,7 @@ def create_app(config_name):
     from .main import main as main_blueprint
     
     bootstrap.init_app('self',app)
+    app.config.from_object(config_options[config_name])
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
